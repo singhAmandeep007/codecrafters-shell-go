3d5c18644bd38fca0998cd774d2733e3c05cd047
Amandeep Singh <55043082+singhAmandeep007@users.noreply.github.com>
2024-12-07 00:46:59 +0530

    implement echo builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -19,9 +20,17 @@ func main() {
 		// The underscore _ is used to ignore any error returned by ReadString.
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-		// If the user enters the exit command, the shell will print a message and exit.
+		// If the user enters the exit command, the shell will exit.
 		if input == "exit 0\n" {
 			break
+		}
+
+		if strings.Split(input, " ")[0] == "echo" {
+			// The echo command is implemented by splitting the input string into words using the strings.Split function.
+			// The first word is the command name, and the rest of the words are the arguments.
+			// The arguments are joined together with a space character and printed to the console.
+			fmt.Println(strings.Join(strings.Split(input, " ")[1:], " "))
+			continue
 		}
 
 		// prints a message indicating that the command is not found.
