85f7c57168ab5fee0b5dda91f08d8f2f0182bdea
Amandeep Singh <55043082+singhAmandeep007@users.noreply.github.com>
2024-12-07 00:38:34 +0530

    implement REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,15 +10,18 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	// reads a line of input from the user and stores it in the variable input.
-	// The underscore _ is used to ignore any error returned by ReadString.
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// prints a message indicating that the command is not found.
-	// The input[:len(input)-1] part removes the newline character from the end of the input string, ensuring the command name is printed correctly without an extra line break.
-	// This change allows the shell to handle invalid commands by displaying a message in the format <command_name>: command not found
-	fmt.Printf("%s: command not found\n", input[:len(input)-1])
+		// Wait for user input
+		// reads a line of input from the user and stores it in the variable input.
+		// The underscore _ is used to ignore any error returned by ReadString.
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		// prints a message indicating that the command is not found.
+		// The input[:len(input)-1] part removes the newline character from the end of the input string, ensuring the command name is printed correctly without an extra line break.
+		// This change allows the shell to handle invalid commands by displaying a message in the format <command_name>: command not found
+		fmt.Printf("%s: command not found\n", input[:len(input)-1])
+	}
 }
