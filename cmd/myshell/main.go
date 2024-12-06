3c4954fdc6977af96e7543572b4499acc5a54a24
Amandeep Singh <55043082+singhAmandeep007@users.noreply.github.com>
2024-12-07 00:58:11 +0530

    refactor

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,29 +14,40 @@ var _ = fmt.Fprint
 func main() {
 
 	for {
-		fmt.Fprint(os.Stdout, "$ ")
+		fmt.Print("$ ")
 
 		// Wait for user input
 		// reads a line of input from the user and stores it in the variable input.
 		// The underscore _ is used to ignore any error returned by ReadString.
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
-		// If the user enters the exit command, the shell will exit.
-		if input == "exit 0\n" {
-			break
+		if err != nil {
+			os.Exit(1)
 		}
 
-		if strings.Split(input, " ")[0] == "echo" {
+		input = strings.TrimSpace(input)
+		commands := strings.Split(input, " ")
+
+		switch commands[0] {
+		case "exit":
+			// The exit command is implemented by checking if the first word of the input is "exit" and the second word is "0".
+			code, err := strconv.Atoi(commands[1])
+			if err != nil {
+				// shell will exit with a non-zero exit code.
+				os.Exit(1)
+			}
+			// shell will successfully exit, zero exit code.
+			os.Exit(code)
+		case "echo":
 			// The echo command is implemented by splitting the input string into words using the strings.Split function.
 			// The first word is the command name, and the rest of the words are the arguments.
 			// The arguments are joined together with a space character and printed to the console.
 			fmt.Print(strings.Join(strings.Split(input, " ")[1:], " "))
-			continue
+		default:
+			// prints a message indicating that the command is not found.
+			// The input[:len(input)-1] part removes the newline character from the end of the input string, ensuring the command name is printed correctly without an extra line break.
+			// This change allows the shell to handle invalid commands by displaying a message in the format <command_name>: command not found
+			fmt.Printf("%s: command not found\n", input[:len(input)-1])
 		}
-
-		// prints a message indicating that the command is not found.
-		// The input[:len(input)-1] part removes the newline character from the end of the input string, ensuring the command name is printed correctly without an extra line break.
-		// This change allows the shell to handle invalid commands by displaying a message in the format <command_name>: command not found
-		fmt.Printf("%s: command not found\n", input[:len(input)-1])
 	}
 }
