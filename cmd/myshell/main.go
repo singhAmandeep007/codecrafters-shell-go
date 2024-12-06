dd78bb4dfb9e05cba9c91dbf3343d37af4fe6c75
Amandeep Singh <55043082+singhAmandeep007@users.noreply.github.com>
2024-12-07 00:41:41 +0530

    implement exit builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -19,6 +19,11 @@ func main() {
 		// The underscore _ is used to ignore any error returned by ReadString.
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
+		// If the user enters the exit command, the shell will print a message and exit.
+		if input == "exit 0\n" {
+			break
+		}
+
 		// prints a message indicating that the command is not found.
 		// The input[:len(input)-1] part removes the newline character from the end of the input string, ensuring the command name is printed correctly without an extra line break.
 		// This change allows the shell to handle invalid commands by displaying a message in the format <command_name>: command not found
